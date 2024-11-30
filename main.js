3a5790aee6c5442340ef7d22b3a468ece07faa57
Tim Orlov <crazy.paradoxer@gmail.com>
2024-12-01 00:36:30 +0300

    chore: reorganize project structure

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
 require('dotenv').config();
 const { Telegraf } = require('telegraf');
 const { message } = require('telegraf/filters');
+// const {exec} = require("child_process");
+
+import { rebootServer, shutdownServer, analyzeServer } from "./src/module-management.js";
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
+// placeholder for tests
 bot.start((ctx) => ctx.reply('Welcome'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+
 bot.launch();
+
+// Management commands
+bot.command('reboot', (ctx) => {
+    rebootServer(ctx);
+});
+
+bot.command('shutdown', (ctx) => {
+    shutdownServer(ctx);
+});
+
+bot.command('stats', (ctx) => {
+    analyzeServer(ctx);
+});
+
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
