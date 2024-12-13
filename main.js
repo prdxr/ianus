f41bade86bed01ed6acbbaa91805d8624d19fcd8
Tim Orlov <crazy.paradoxer@gmail.com>
2024-12-13 07:26:28 +0300

    feat: add post files function

--- main.js
+++ main.js
@@ -3,10 +3,11 @@ import { Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
 
 import { rebootServer, shutdownServer, analyzeServer, executeCustomCommand } from "./src/module-management.js";
-import {getFromShare, listShare} from "./src/module-filestorage.js";
+import {getFromShare, listShare, postToShare} from "./src/module-filestorage.js";
 
 dotenv.config();
 const bot = new Telegraf(process.env.BOT_TOKEN);
+let waitForFile = false;
 
 // placeholder for tests
 bot.start((ctx) => ctx.reply('Welcome'));
@@ -42,7 +43,20 @@ bot.command('get', (ctx) => {
     getFromShare(ctx, ctx.message.text.slice(5));
 })
 
+bot.command('post', (ctx) => {
+    ctx.reply('Please provide the file you want to upload.');
 
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+    waitForFile = true;
+})
+
+bot.on('document', (ctx) => {
+    if (!waitForFile) { return }
+
+    waitForFile = false;
+    let fileId = ctx.message.document.file_id;
+    postToShare(ctx, fileId);
+})
+
+// // Enable graceful stop
+// process.once('SIGINT', () => bot.stop('SIGINT'));
+// process.once('SIGTERM', () => bot.stop('SIGTERM'))
--- src/module-filestorage.js
+++ src/module-filestorage.js
@@ -1,6 +1,7 @@
 import { exec } from "node:child_process";
 import fs from "fs";
 import path from "path";
+import axios from "axios";
 
 // list share command
 export function listShare(ctx) {
@@ -15,7 +16,6 @@ export function listShare(ctx) {
 // get file command
 export function getFromShare(ctx, filename) {
     let filepath = path.join(process.env.SHARE_PATH, filename);
-    console.log(filepath);
     fs.readFile(filepath, (err, data) => {
         if (!err) {
             console.log('received data: ' + data);
@@ -30,3 +30,40 @@ export function getFromShare(ctx, filename) {
 }
 
 // post file command
+export function postToShare(ctx, fileId) {
+    let filepath = path.join(
+        process.env.SHARE_PATH,
+        ctx.message.document.file_name
+    );
+
+    console.log(fileId, ctx.telegram.getFileLink(fileId));
+
+
+    ctx.telegram.getFileLink(fileId).then((link) => {
+        console.log(link);
+        let linkStr = link.href;
+        console.log(`link str: ${linkStr}`);
+        axios({
+            url: linkStr,
+            method: 'get',
+            responseType: 'stream'
+        }).then(response => {
+            return new Promise((resolve, reject) => {
+                response.data.pipe(fs.createWriteStream(filepath))
+                    .on('finish', () => {
+                        ctx.reply(`Document is saved at ${filepath}`);
+                        resolve();
+                    })
+                    .on('error', (err) => {
+                        reject(err);
+                    });
+            });
+        }).catch(error => {
+            console.error("Axios request error: ", error);
+            ctx.reply("Failed to download the file.");
+        });
+    }).catch(err => {
+        console.error("Get file link error: ", err);
+        ctx.reply("Failed to retrieve the file link.");
+    });
+}
