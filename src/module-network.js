c631b92afb82ea162d962f6ddef02acf93f17b09
Tim Orlov <crazy.paradoxer@gmail.com>
2024-12-14 06:21:17 +0300

    feat: add network scan function

--- src/module-network.js
+++ src/module-network.js
@@ -1,7 +1,9 @@
 import { wake } from 'wake_on_lan';
 import { networkInterfaces } from 'os';
 import { createSession } from 'net-ping';
+import {exec} from "node:child_process";
 
+// wake-on-lan command
 export function wakeUpComputer(macAddress) {
     return new Promise((resolve, reject) => {
         wake(macAddress, (error) => {
@@ -16,47 +18,12 @@ export function wakeUpComputer(macAddress) {
     });
 }
 
-function getLocalSubnet() {
-    const interfaces = networkInterfaces();
-    for (const name of Object.keys(interfaces)) {
-        for (const iface of interfaces[name]) {
-            if (iface.family === 'IPv4' && !iface.internal) {
-                const subnet = iface.address.split('.').slice(0, 3).join('.') + '.0/24';
-                return subnet;
-            }
+// scan network command
+export function scanNetwork(ctx) {
+    exec('nmap -sn 192.168.1.0/24', (err, stdout) => {
+        if (err) {
+            return ctx.reply(`Error scanning: ` + err.message);
         }
-    }
-    throw new Error('Unable to determine local subnet');
+        ctx.reply(`Result:\n${stdout}`);
+    });
 }
-
-export function scanNetwork() {
-    return undefined;
-}
-//     const session = createSession();
-//     const subnet = getLocalSubnet();
-//     const baseIp = subnet.split('/')[0].split('.').slice(0, 3).join('.');
-//     const promises = [];
-//
-//     console.log(`Scanning subnet: ${subnet}`);
-//
-//     for (let i = 1; i <= 254; i++) {
-//         const ip = `${baseIp}.${i}`;
-//         promises.push(
-//             new Promise(resolve => {
-//                 session.pingHost(ip, (error, target) => {
-//                     if (!error) {
-//                         resolve(target); // Responding device
-//                     } else {
-//                         resolve(null); // No response
-//                     }
-//                 });
-//             })
-//         );
-//     }
-//
-//     Promise.all(promises).then(results => {
-//         const activeDevices = results.filter(Boolean);
-//         ctx.(activeDevices); // Devices that responded
-//         session.close();
-//     });
-// }
