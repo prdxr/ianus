b4a591c66c960ec11a653a5804d1cc296b1a9515
Tim Orlov <crazy.paradoxer@gmail.com>
2024-12-14 04:06:49 +0300

    fix: change DHT-11 logic

--- src/module-sensors.js
+++ src/module-sensors.js
@@ -1,17 +1,23 @@
 import dht from 'node-dht-sensor';
 
 const sensorType = 11; // DHT11
-const gpioPin = process.env.DHT_PIN;
+const gpioPin = Number(process.env.DHT_PIN);
+
+dht.initialize(sensorType, gpioPin);
 
 // read humidity-temperature function
 export function readDhtSensor() {
     return new Promise((resolve, reject) => {
-        dht.read(sensorType, gpioPin, (err, temperature, humidity) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve({ temperature, humidity });
-            }
-        });
+        try {
+            const readout = dht.read();
+            const temperature = readout.temperature.toFixed(1);
+            const humidity = readout.humidity.toFixed(1);
+
+            // Respond with sensor data
+            resolve({ temperature, humidity });
+        } catch (error) {
+            console.error('Error reading sensor data:', error);
+            reject(error);
+        }
     });
 }
